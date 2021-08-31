e72afcaa75aa575edf73ba926bb348fdcaefb64a
Alain Buisseret <me@alain.dev>
2021-08-31 13:55:51 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,26 @@
+use seed::{*, prelude::*};
+
+pub enum Msg {
+	NoMsg
+}
+
+struct Model {}
+
+fn init(_url: Url, _orders: &mut impl Orders<Msg>) -> Model {
+    Model {}
+}
+fn update(msg: Msg, _model: &mut Model, orders: &mut impl Orders<Msg>) {
+    orders.skip();
+    match msg {
+		Msg::NoMsg => {}
+	}
+}
+fn view(_model: &Model) -> Vec<Node<Msg>> {
+    nodes!["Hello world !"]
+}
+
+#[wasm_bindgen(start)]
+pub fn start() {
+    // Mount the `app` to the element with the `id` "app".
+    App::start("app", init, update, view);
+}
